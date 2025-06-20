a317a29867a45cc51e078e35ca9ea02f180511d1
Asilbek <asilbeksafarov962@gmail.com>
2025-06-20 10:56:12 +0500

    First commit

---
+++ src/main/java/dasturlashasil/uz/KunUzProApplication.java
@@ -0,0 +1,13 @@
+package dasturlashasil.uz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KunUzProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KunUzProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dasturlashasil/uz/KunUzProApplicationTests.java
@@ -0,0 +1,13 @@
+package dasturlashasil.uz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KunUzProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
