4d419ac101699404d06d3451d90ef4470eb16178
Asilbek <asilbeksafarov962@gmail.com>
2025-06-24 08:35:59 +0500

    Odin20 commit : Auth: Registraton Token connect

--- src/main/java/dasturlashasil/uz/controller/AuthController.java
+++ src/main/java/dasturlashasil/uz/controller/AuthController.java
@@ -3,13 +3,11 @@ package dasturlashasil.uz.controller;
 
 import dasturlashasil.uz.Dto.auth.RegistrationDto;
 import dasturlashasil.uz.service.AuthService;
+//import dasturlashasil.uz.service.AuthServiceV2bbbb;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -23,6 +21,16 @@ public class AuthController {
     public ResponseEntity<String> registration(@Valid @RequestBody RegistrationDto dto) {
         return ResponseEntity.ok(authService.registration(dto));
     }
+
+    @GetMapping("/registration/email/verification/{token}") //generalmidi?
+    public ResponseEntity<String> registration( @PathVariable("token") String token) {
+        return ResponseEntity.ok(authService.regEmailVerification(token));
+    }
+
+//    @GetMapping("/registration/email/verification/{username}/{code}") //registration uchun
+//    public ResponseEntity<String> registration(@PathVariable("username") String username, @PathVariable("code") int code ){
+//        return ResponseEntity.ok(authService.regEmailVerification(username,code));
+//    }
 
 
 
