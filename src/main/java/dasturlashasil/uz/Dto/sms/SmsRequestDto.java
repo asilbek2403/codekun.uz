d68f591ce06219c28795157bab7b2f98f4cca908
Asilbek <asilbeksafarov962@gmail.com>
2025-06-25 17:37:46 +0500

    Odin26 commit :SmsHistory (TokenProviderResponse) RequestDto

---
+++ src/main/java/dasturlashasil/uz/Dto/sms/SmsRequestDto.java
@@ -0,0 +1,12 @@
+package dasturlashasil.uz.Dto.sms;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SmsRequestDto {
+
+    private String mobile_phone;
+    private String message;
+}
---
+++ src/main/java/dasturlashasil/uz/Dto/sms/SmsTokenProviderResponse.java
@@ -0,0 +1,18 @@
+package dasturlashasil.uz.Dto.sms;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SmsTokenProviderResponse {
+    private String message;
+    private Data data;
+    private String token_type;
+
+    @Getter
+    @Setter
+    public static class Data {
+        private String token;
+    }
+}
