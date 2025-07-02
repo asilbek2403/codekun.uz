34e683fce92e4d79c05ee4abb5dc79efb5b38c5d
Asilbek <asilbeksafarov962@gmail.com>
2025-07-02 06:41:52 +0500

    Odin33 commit : UserDetails Find username for USER and Security Return CustomUserDetails(profile...,roles)

---
+++ src/main/java/dasturlashasil/uz/config/CustomUserDetailsService.java
@@ -0,0 +1,47 @@
+package dasturlashasil.uz.config;
+
+
+import dasturlashasil.uz.Enums.ProfileRoleEnum;
+import dasturlashasil.uz.entities.ProfileEntity;
+import dasturlashasil.uz.entities.ProfileRoleEntity;
+import dasturlashasil.uz.repository.ProfileRepository;
+import dasturlashasil.uz.repository.ProfileRoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+//import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    //  "username": "asilustoz37@gmail.com",
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    @Autowired
+    private ProfileRoleRepository profileRoleRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<ProfileEntity> optional = profileRepository.findByUsernameAndVisibleIsTrue(username);
+        if( optional.isEmpty()){
+            throw new UsernameNotFoundException(username+ "< User >not found");
+        }
+        ProfileEntity profile = optional.get();
+        List<ProfileRoleEnum> roleEntities = profileRoleRepository.getRoleListByProfileId(profile.getId());
+
+        return new CustomUserDetails(profile.getId() ,
+                profile.getUsername() ,
+                profile.getPassword(),
+                profile.getProfileStatus(),
+                roleEntities);
+
+    }
+
+}
