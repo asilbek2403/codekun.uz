6255f4fb9ac68df1465c0f30506c6755f0b1fb46
Asilbek <asilbeksafarov962@gmail.com>
2025-07-02 06:40:07 +0500

    Odin32 commit : private Int id username password; ProfStatusEn statusEnum;<SimpleGrantedAuthority> roles; Methods: >> statusEnum.equals(ProfileStatusEnum.ACTIVE) AND  public Collection<? extends GrantedAuthority> getAuthorities() {         return roles;     }

---
+++ src/main/java/dasturlashasil/uz/config/CustomUserDetails.java
@@ -0,0 +1,76 @@
+package dasturlashasil.uz.config;
+
+
+import dasturlashasil.uz.Enums.ProfileRoleEnum;
+import dasturlashasil.uz.Enums.ProfileStatusEnum;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private Integer id;
+    private String username;
+    private String password;//Profile ni umuman aralashtirmadik
+    private ProfileStatusEnum statusEnum;
+    private  List<SimpleGrantedAuthority> roles;
+
+    public CustomUserDetails(Integer id, String username,
+                             String password,
+                             ProfileStatusEnum statusEnum,
+                             List<ProfileRoleEnum> roles) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.statusEnum = statusEnum;
+        List<SimpleGrantedAuthority> roleList = new ArrayList<>();
+        roles.forEach(role -> {
+            roleList.add( new SimpleGrantedAuthority(role.name()));
+        });
+        this.roles = roleList;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true; // UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return  statusEnum.equals(ProfileStatusEnum.ACTIVE); //UserDetails.super.isAccountNonLocked();
+    }
+
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return  true; //UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return  true; //UserDetails.super.isEnabled();
+    }
+
+
+}
