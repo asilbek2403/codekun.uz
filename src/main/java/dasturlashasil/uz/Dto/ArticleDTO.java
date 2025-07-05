6bb806ad6f8e58670a531b8fc23eebafe4ca4555
Asilbek <asilbeksafarov962@gmail.com>
2025-07-05 07:13:59 +0500

    Odin44 com :[ id;  title description content;sharedCount; imageId;  regionId  moderatorId publisherId articleStatus   readTime;viewCount; publishedDate List<C> categoryL , L<S> sectionList              ]
    Faqat Frontend>>>> Bsckend holida

---
+++ src/main/java/dasturlashasil/uz/Dto/ArticleDTO.java
@@ -0,0 +1,37 @@
+package dasturlashasil.uz.Dto;
+
+import dasturlashasil.uz.Enums.ArticleStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+public class ArticleDTO {
+    private String id;
+    private String title;
+    private String description;
+    private String content;
+    private Long sharedCount;
+
+    private String imageId;
+    private Integer regionId;
+
+    private Integer moderatorId;
+
+    private Integer publisherId;
+
+    private ArticleStatus status;
+
+    private Integer readTime; // in second
+    private Integer viewCount; // in second
+    private LocalDateTime publishedDate;
+
+
+    private List<CategoryDto> categoryList;
+    private List<SectionDto> sectionList;
+
+
+}
