a65b92a9f63546dc3bfd8d23a34b015c10a56d69
Asilbek <asilbeksafarov962@gmail.com>
2025-07-10 15:13:05 +0500

    Super70 -- commmit : ArticleFilter (  title regionId ...(12))

---
+++ src/main/java/dasturlashasil/uz/Dto/ArticleFilterDTO.java
@@ -0,0 +1,17 @@
+package dasturlashasil.uz.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class ArticleFilterDTO {
+    private String title;
+    private Integer regionId;
+    private Integer categoryId;
+    private Integer sectionId;
+    private LocalDate createdDateFrom;
+    private LocalDate createdDateTo;
+}
---
+++ src/main/java/dasturlashasil/uz/Dto/Last12ArticleDTO.java
@@ -0,0 +1,14 @@
+package dasturlashasil.uz.Dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class Last12ArticleDTO {
+    @NotEmpty(message = "Except article id list required")
+    private List<String> exceptArticleIdList;
+}
