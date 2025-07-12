e310d9074e6f57b715e11500ef3e8b2457930765
Asilbek <asilbeksafarov962@gmail.com>
2025-07-12 19:07:44 +0500

    Super72-2 commit:{RegionMapper}     Optional<RegionMapper> getByIdAndLang(@Param("id") Integer id, @Param("lang") String lang);

---
+++ src/main/java/dasturlashasil/uz/mapperL/RegionMapper.java
@@ -0,0 +1,12 @@
+package dasturlashasil.uz.mapperL;
+
+
+public interface RegionMapper {
+    Integer getId();
+
+    String getName();
+
+    Integer getOrderNumber();
+
+    String getKey();
+}
--- src/main/java/dasturlashasil/uz/repository/RegionRepository.java
+++ src/main/java/dasturlashasil/uz/repository/RegionRepository.java
@@ -1,7 +1,10 @@
 package dasturlashasil.uz.repository;
 
 import dasturlashasil.uz.entities.RegionEntity;
+import dasturlashasil.uz.mapperL.RegionMapper;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,5 +17,16 @@ public interface RegionRepository extends CrudRepository<RegionEntity, Integer> 
 
     Optional<RegionEntity> findByOrderNum(Integer number);
 
+
+    @Query("SELECT c.id AS id, " +
+            "CASE :lang " +
+            "   WHEN 'UZ' THEN c.nameUz " +
+            "   WHEN 'RU' THEN c.nameRu " +
+            "   WHEN 'EN' THEN c.nameEn " +
+            "END AS name, " +
+            "c.key AS regionKey " +
+            "FROM RegionEntity c " +
+            "WHERE c.visible = true and c.id = :id")
+    Optional<RegionMapper> getByIdAndLang(@Param("id") Integer id, @Param("lang") String lang);
 }
 
