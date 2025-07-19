46a03815bd9aa2abe9c641954c0d90b0a2062979
Asilbek <asilbeksafarov962@gmail.com>
2025-07-19 18:16:01 +0500

    Super87 commit:      int deleteByArticleIdAndProfileId(String articleId, Integer profileId);

---
+++ src/main/java/dasturlashasil/uz/repository/ArticleLikeRepository.java
@@ -0,0 +1,19 @@
+package dasturlashasil.uz.repository;
+
+import dasturlashasil.uz.entities.ArticleLikeEntity;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface ArticleLikeRepository extends CrudRepository<ArticleLikeEntity, String> {
+
+    Optional<ArticleLikeEntity> findByArticleIdAndProfileIdAndVisibleTrue(String articleId, Integer profileId);
+
+    @Transactional
+    @Modifying
+    @Query("update ArticleLikeEntity  set visible = false, deletedDate = current_timestamp where articleId =?1 and profileId =?2 and visible = true")
+    int deleteByArticleIdAndProfileId(String articleId, Integer profileId);
+}
