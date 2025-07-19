c668d1adadb6fe0723c1bf4c0cc3bb2b57721d8b
Asilbek <asilbeksafarov962@gmail.com>
2025-07-19 18:19:31 +0500

    Super89-9 commit:  CommentService hasRole(PrEnum...roles)

--- src/main/java/dasturlashasil/uz/util/SpringSecurityUtil.java
+++ src/main/java/dasturlashasil/uz/util/SpringSecurityUtil.java
@@ -1,8 +1,14 @@
 package dasturlashasil.uz.util;
 
+import dasturlashasil.uz.Enums.ProfileRoleEnum;
 import dasturlashasil.uz.config.CustomUserDetails;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Arrays;
+import java.util.List;
+
 //
 //
 //public class SpringSecurityUtil {
@@ -32,4 +38,23 @@ public class SpringSecurityUtil {
 
         return null; // yoki throw new AppBadException("Principal type unknown")
     }
+
+
+    //Comment Service
+
+    public static Boolean hasAnyRoles(ProfileRoleEnum... roles) {
+        List<String> roleList = Arrays.stream(roles).map(Enum::name).toList();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails user = (CustomUserDetails) authentication.getPrincipal();
+        for (GrantedAuthority authority : user.getAuthorities()) {
+            if (roleList.contains(authority.getAuthority())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
 }
