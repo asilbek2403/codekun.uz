da88a7e7fb2e21a9a71ec0c26f7c236aaefa67a7
Asilbek <asilbeksafarov962@gmail.com>
2025-07-24 10:44:08 +0500

    Super92 commit :    MigrationConfiguration  dataSourceUrl dataSourceUsername  dataSourcePassword

---
+++ src/main/java/dasturlashasil/uz/config/MigrationConfiguration.java
@@ -0,0 +1,37 @@
+package dasturlashasil.uz.config;
+
+
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+// Base bilan ishlaydigan faylda Comandalarni yozamiz MIGRATSIYA PAPKASI
+@Configuration
+public class MigrationConfiguration {
+    @Value("${spring.datasource.url}")
+    private String dataSourceUrl;
+    @Value("${spring.datasource.username}")
+    private String dataSourceUsername;
+    @Value("${spring.datasource.password}")
+    private String dataSourcePassword;
+
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url(dataSourceUrl);
+        dataSourceBuilder.username(dataSourceUsername);
+        dataSourceBuilder.password(dataSourcePassword);
+        return dataSourceBuilder.build();
+    } // qaysi baza bilan ishlashini o'zida olib turadi
+
+}
+
