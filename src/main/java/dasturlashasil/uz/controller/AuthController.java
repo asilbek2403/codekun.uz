446c8649c6072c8beb2c6b02fc96e93e46588d0c
Asilbek <asilbeksafarov962@gmail.com>
2025-07-24 10:38:52 +0500

    Super91 commit: AuthController Just Logging()

--- src/main/java/dasturlashasil/uz/controller/AuthController.java
+++ src/main/java/dasturlashasil/uz/controller/AuthController.java
@@ -6,21 +6,38 @@ import dasturlashasil.uz.Dto.auth.RegistrationDto;
 import dasturlashasil.uz.Dto.profile.ProfileDto;
 import dasturlashasil.uz.service.AuthService;
 //import dasturlashasil.uz.service.AuthServiceV2bbbb;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+//import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+//import java.util.logging.Logger;
+
 @RestController
 @RequestMapping("/api/auth")
+@Tag(name = "Auth APIs", description = "API list for managing Authorization and Authentication")
 public class AuthController {
 
         @Autowired
         private AuthService authService;
 
+//    private static Logger log = LoggerFactory.getLogger(AuthController.class);
 
     @PostMapping("/registration")
     public ResponseEntity<String> registration(@Valid @RequestBody RegistrationDto dto) {
+//        log.debug("Debug log");
+//        log.info("Info log"); // INFO
+//        log.warn("Warning log")
+//
+//
+//        ;
+//        log.error("Error log");
+
         return ResponseEntity.ok(authService.registration(dto));
     }
 
@@ -35,6 +52,7 @@ public class AuthController {
 //    }
 
     @PostMapping("/login")
+    @Operation(summary = "Authorization", description = "Api used for Authorization")
     public ResponseEntity<ProfileDto> login(@Valid @RequestBody AuthorizationDto dto) {
         return ResponseEntity.ok(authService.login(dto));
     }
