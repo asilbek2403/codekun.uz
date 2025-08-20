b6fbbf2d92b167836b2f443d257152c6f973aa14
Asilbek <asilbeksafarov962@gmail.com>
2025-08-20 22:05:00 +0500

    Super98-8 commit : CommentMapper

---
+++ src/main/java/dasturlashasil/uz/mapperL/CommentMapper.java
@@ -0,0 +1,27 @@
+package dasturlashasil.uz.mapperL;
+
+
+import java.time.LocalDateTime;
+
+public interface CommentMapper {
+    String getId();
+
+    String getContent();
+
+    LocalDateTime getCreatedDate();
+
+    LocalDateTime getUpdatedDate();
+
+    Integer getProfileId();
+
+    String getProfileName();
+
+    String getProfileSurname();
+
+    String getProfilePhotoId();
+
+    Long getLikeCount();
+
+    Long getDislikeCount();
+
+}
